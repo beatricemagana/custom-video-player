791fbb21b421c068102f50f28984cc9522fad688
beatricemagana <beemag06@gmail.com>
2021-03-23 20:31:23 -0700

    add play and pause function, add function to change play and pause icon

--- script.js
+++ script.js
@@ -4,6 +4,40 @@ const stop = document.getElementById('stop');
 const progress = document.getElementById('progress');
 const timestamp = document.getElementById('timestamp');
 
+
+// play and pause 
+function toggleVideoStatus() {
+    if(video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+//update play and pause icon 
+function updatePlayIcon() {
+    if(video.paused){
+        play.innerHTML = '<i class ="fa fa-play fa-2x"></i>';
+    } else {
+        play.innerHTML = '<i class ="fa fa-pause fa-2x"></i>';
+    }
+}
+
+//update progress and timestamp 
+function updateProgress() {
+    return true;
+}
+
+// set video time to progress 
+function setVideoProgress() {
+    return true;
+}
+
+// stop video 
+function stopVideo() {
+    return true;
+}
+
 // event listeners 
 video.addEventListener('click', toggleVideoStatus);
 video.addEventListener('pause', updatePlayIcon);
